9e0b13a8bce05b4b95f6bd07df835255f035fecb
Elsti97 <elsti.shehati@hotmail.it>
2023-03-17 14:23:06 +0100

    Base di questa mattina

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] numeri = { 2, 6, 7, 5, 3, 9 };
+
+printArray(numeri);
+
+void printArray(int[] array)
+{
+    Console.WriteLine(arrayToString(array));
+}
+
+string arrayToString(int[] array)
+{
+    var result = "[";
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += array[i].ToString();
+
+        if (i < array.Length - 1)
+        {
+            result += ", ";
+        }
+    }
+
+    result += "]";
+
+    return result;
+}
