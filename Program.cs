97c4fd6e4dafe36944e9d93aa881eb4259bb28e9
Elsti97 <elsti.shehati@hotmail.it>
2023-03-17 14:53:23 +0100

    Multiply and sum

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
-﻿int[] numeri = { 2, 6, 7, 5, 3, 9 };
-
+﻿
+// Array vari e funzione stampa riutilizzata
+int[] numeri = { 2, 6, 7, 5, 3, 9 };
+Console.Write("Array originale: ");
 printArray(numeri);
 
+int[] numeriAlQuadrato = ElevaArrayAlQuadrato(numeri);
+Console.Write("Array al quadrato: ");
+printArray(numeriAlQuadrato);
+
+int somma = sommaElementiArray(numeri);
+Console.WriteLine("La somma è: " + somma);
+
+// Funzione stampa array
 void printArray(int[] array)
 {
     Console.WriteLine(arrayToString(array));
 }
 
+// Funzione calcolo quadrato
+int Quadrato(int numeroSingolo)
+{
+    return numeroSingolo * numeroSingolo;
+}
+
+// Funzione Array in stringa
 string arrayToString(int[] array)
 {
     var result = "[";
@@ -24,4 +41,29 @@ string arrayToString(int[] array)
     result += "]";
 
     return result;
+}
+
+// Funzione elevazione al quadrato di array clonato
+int[] ElevaArrayAlQuadrato(int[] array)
+{
+    int[] copiaArray = (int[])array.Clone();
+    int[] risultato = new int[copiaArray.Length];
+    for (int i = 0; i < copiaArray.Length; i++)
+    {
+        risultato[i] = Quadrato(copiaArray[i]);
+    }
+    return risultato;
+}
+
+// Funzione somma di un array
+int sommaElementiArray(int[] array)
+{
+    int somma = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        somma += array[i];
+    }
+
+    return somma;
 }
